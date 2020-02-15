be3e11a138c5e278c4f074342784dfc616fd95bb
drego <danny@tucows.com>
2020-02-15 15:47:13 -0500

    Adjusted the main INO file to the new interface to add buttons

--- midi-ble-footswitch.ino
+++ midi-ble-footswitch.ino
@@ -14,16 +14,23 @@ void setup() {
 
   // Add all of our controls for this pedal...
   // 4 midi note buttons (input pins: 13, 15, 17, 19, led pins: 12, 14, 16, 18)
-  controllerObj->addButton(13, 12, MIDI_CHANNEL_1, MIDI_NOTE_E3, BUTTON_PRESS_TYPE_MOMENTARY);
-  controllerObj->addButton(15, 14, MIDI_CHANNEL_1, MIDI_NOTE_F3, BUTTON_PRESS_TYPE_MOMENTARY);
-  controllerObj->addButton(17, 16, MIDI_CHANNEL_1, MIDI_NOTE_Gb3, BUTTON_PRESS_TYPE_MOMENTARY);
-  controllerObj->addButton(19, 18, MIDI_CHANNEL_1, MIDI_NOTE_G3, BUTTON_PRESS_TYPE_MOMENTARY);
+  controllerObj->addCCButton(13, 12, MIDI_CHANNEL_1, MIDI_CC_CONTROL_20);
+  controllerObj->addCCButton(15, 14, MIDI_CHANNEL_1, MIDI_CC_CONTROL_21);
+  controllerObj->addCCButton(17, 16, MIDI_CHANNEL_1, MIDI_CC_CONTROL_22);
+  controllerObj->addCCButton(19, 18, MIDI_CHANNEL_1, MIDI_CC_CONTROL_23);
+
+  // If you would rather have the buttons represent notes being played...
+  //controllerObj->addNoteButton(19, 18, MIDI_CHANNEL_1, MIDI_NOTE_E3, BUTTON_PRESS_TYPE_MOMENTARY);
+  //controllerObj->addNoteButton(19, 18, MIDI_CHANNEL_1, MIDI_NOTE_F3, BUTTON_PRESS_TYPE_MOMENTARY);
+  //controllerObj->addNoteButton(19, 18, MIDI_CHANNEL_1, MIDI_NOTE_Gb3, BUTTON_PRESS_TYPE_LATCHING);
+  //controllerObj->addNoteButton(19, 18, MIDI_CHANNEL_1, MIDI_NOTE_G3, BUTTON_PRESS_TYPE_LATCHING);
+
   // ...and 2 midi expression pedals (input pins: 2, and 3)
-  controllerObj->addExpressionPedal(2, MIDI_CHANNEL_1, EXPRESSION_PEDAL_DIRECTION_LOW_TO_HIGH, EXPRESSION_PEDAL_CURVE_LINEAR);
-  controllerObj->addExpressionPedal(3, MIDI_CHANNEL_2, EXPRESSION_PEDAL_DIRECTION_LOW_TO_HIGH, EXPRESSION_PEDAL_CURVE_LINEAR);
+  controllerObj->addExpressionPedal(2, MIDI_CHANNEL_1, MIDI_CC_CONTROL_12, EXPRESSION_PEDAL_DIRECTION_LOW_TO_HIGH, EXPRESSION_PEDAL_CURVE_LINEAR);
+  controllerObj->addExpressionPedal(3, MIDI_CHANNEL_2, MIDI_CC_CONTROL_13, EXPRESSION_PEDAL_DIRECTION_LOW_TO_HIGH, EXPRESSION_PEDAL_CURVE_LINEAR);
 
   // ...and while we are testing, setup the onboard button/led as a note button...
-  //controllerObj->addButton(6, 7, MIDI_CHANNEL_1, MIDI_NOTE_C3, BUTTON_PRESS_TYPE_LATCHING);
+  //controllerObj->addNoteButton(6, 7, MIDI_CHANNEL_1, MIDI_NOTE_C3, BUTTON_PRESS_TYPE_LATCHING);
 
   controllerObj->start();
 
